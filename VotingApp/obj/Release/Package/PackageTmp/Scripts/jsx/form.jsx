fe7e64292c27441a5553f73f0997241317355c9c
Jacob A Buckley <s292155@corp.aepsc.com>
2019-03-04 16:53:11 -0500

    current version update not sure what ll changed maybe everything.

---
+++ VotingApp/obj/Release/Package/PackageTmp/Scripts/jsx/form.jsx
@@ -0,0 +1,123 @@
+﻿var RadioInput = React.createClass( {
+    handleClick: function() {
+        this.props.onChoiceSelect( this.props.choice );
+    },
+    render: function() {
+        var disable = this.props.disable;
+        var classString = !disable ?  "radio" :  "radio disabled";
+        return (
+            <div className={classString}>
+                <label className={this.props.classType}>
+                    <input type="radio" name="optionsRadios" id={this.props.index} value={this.props.choice} onChange={this.handleClick}  />
+                    {this.props.choice}
+                </label>
+            </div>
+        );
+    }
+} );
+
+
+var QuizContainer = React.createClass( {
+    getInitialState: function() {
+        return {     
+            //current_quiz: {question: '', choices: [] },
+            //these are the elements we passed in with the homeController.cs
+            current_quiz: { Header : '', question : '', voteTxt : '' , choices : [] },//to make this even more modular you could pass in more elements from the homeController
+            user_choice: "", 
+			is_done: false           
+        };
+    },
+
+    /*
+    componentDidMount() is invoked immediately after a component is mounted (inserted into the tree). Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
+
+This method is a good place to set up any subscriptions. If you do that, don’t forget to unsubscribe in componentWillUnmount().
+
+You may call setState() immediately in componentDidMount().
+It will trigger an extra rendering, but it will happen before the browser updates the screen. 
+This guarantees that even though the render() will be called twice in this case, the user won’t see the intermediate state. 
+Use this pattern with caution because it often causes performance issues. 
+In most cases, you should be able to assign the initial state in the constructor() instead. 
+It can, however, be necessary for cases like modals and tooltips when you need to measure a DOM node before rendering something that depends on its size or position.
+*/
+	componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState( {           
+            current_quiz: data,
+            user_choice: "",
+			is_done: false           
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+   },
+    selectedAnswer: function( option ) {
+        this.setState( { user_choice: option } );
+    },
+    handleSubmit: function() {           			
+
+				var selectedChoice = this.state.user_choice;
+				 var vhub = $.connection.votingHub;
+				  $.connection.hub.start().done(function () {				
+                    // Call the Send method on the hub.
+                    vhub.server.send(selectedChoice);
+                    // Clear text box and reset focus for next comment.                   
+				});
+				this.setState({ is_done: true });
+        },
+    render: function() {
+        var self = this;
+
+        if (this.state.is_done === true) {
+            setTimeout(function(){window.location.href = "/home";}, 1000); //Set time out(Action,Time) Im using action to redirect home
+			return (
+				<div className="quizContainer">
+					<h1>Thank you for your vote. </h1>
+				</div>
+			);
+		}
+		else 
+		{
+        var choices = this.state.current_quiz.choices.map( function( choice, index ) {           
+            return (
+                <RadioInput key={choice.name} choice={choice.name} index={index} onChoiceSelect={self.selectedAnswer} />
+            );
+            });
+            
+            var button_name = this.state.current_quiz.voteTxt; //this makes the vote button text dynamic
+
+            //so here the meat of out page
+            //its a container -> header -> paragraph -> 
+            //folowed by the {choices} table
+            //then because we want the vote button fixed on the bottom we set up another navbar and toss a button inside it
+            return (
+                <div className="Container1">
+
+                    <div className="quizContainer">
+                        <h1>{this.state.current_quiz.Header}</h1>
+                        <p>{this.state.current_quiz.question}</p>
+                        {choices}                          
+                    </div>
+
+                    <div className="navbar nb navbar-fixed-bottom"> 
+                        <div className="container">
+                                <button id="submit1" className="btn btn-default" onClick={this.handleSubmit}>{button_name}</button>
+                        </div>
+                    </div>
+
+                </div>
+        );
+
+		}
+    }
+} );
+
+React.render(
+    <QuizContainer url="/home/surveyquiz" />,
+    document.getElementById('container')
+);
---
+++ VotingApp/obj/Release/Package/PackageTmp/Scripts/jsx/piechart.jsx
@@ -0,0 +1,195 @@
+﻿//this is what defines our piechart on the dashboard
+
+var colors = ['#FD9827']; //this color is an artifact of me trying to make a bunch of colors, this is the reason why the app with the most apps is always orange, because its first in the array
+pop(); //pop will make fill out possible colors array for us to use
+
+
+function pop() {
+    for (var i = 0; i < 200; i++) { // from 0, 200 so if you had 200 cramped little options all of them would have a unique...ish color you can boost it almost forever though
+        colors.push(getRandomColor()); //adds the new color the the stack
+    }
+}
+function getRandomColor() {
+    //so a color is a # followed by a hex favlue so here are the hex values
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]; //we take # and then we just append a random assortment of hex... stuff on the back yay new color
+    }
+    return color;
+}//this comparotor is used to sort the location of to objects(say for instance... the number of votes a table has) it returns 1 or or -1
+function compare(a, b) {
+    if (a.count < b.count)
+        return 1;
+    if (a.count > b.count)
+        return -1;
+    return 0;
+}
+
+//here where we start thepie chart stuff
+var D3Legend = React.createClass({
+
+  propTypes: { // properties that a pie chart has
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,//using number for height and width because circles.
+    colors: React.PropTypes.array.isRequired, //this is the array we filled with color
+    data: React.PropTypes.array.isRequired, //this is the json dictionary we will pass in
+  },
+
+  render: function() { //think of this like a constructor
+    var color = this.props.colors;
+    var data = this.props.data;
+    var elements = data.map(function(item, i){
+      return (
+        <LegendElement color={color} xpos="0" ypos={100+i*20} data={item.name} key={i} ikey={i}/>
+      )
+    })
+
+    return(
+        <svg className="legend" width={this.props.width} height={this.props.height}>{elements}</svg>
+    );
+  }
+});
+
+
+
+var LegendElement = React.createClass({
+  render: function() {
+    var position =  "translate(" + this.props.xpos + "," + this.props.ypos + ")";
+      //size of the table is defined in the rect width and height down there
+    return (
+      <g transform={position}>
+        <rect width="18" height="18" fill={this.props.color[this.props.ikey]}></rect>     
+        <text x="24" y="9" dy=".35em">{this.props.data}</text>
+      </g>
+    );
+  }
+});
+
+//this peice of magic decides how big each slice of pie is
+var Sector = React.createClass({
+  getInitialState: function() {
+    return {text: '', opacity:'arc'};
+  },
+  render: function() {
+    var outerRadius = this.props.width/2.2;
+    var innerRadius = this.props.width/8;
+    var arc = d3.svg.arc()
+        .outerRadius(outerRadius)
+        .innerRadius(innerRadius);
+    var data = this.props.data;
+    var center = "translate(" + arc.centroid(data) + ")";
+    var percentCenter = "translate(0,3)";
+    var color = this.props.colors;
+    return (
+      <g onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} onClick={this.onClick}>
+        <path className={this.state.opacity} fill={colors[this.props.ikey]} d={arc(this.props.data)}></path>
+        <text fill="white" transform={center} textAnchor="middle" fontSize="15px">{data.value}</text>
+        <text fill={colors[this.props.ikey]} stroke={color} fontSize="15px" transform={percentCenter} textAnchor="middle">{this.state.text}</text>
+      </g>
+    );
+  },
+
+    //this is for the onHober function that shows % of pie on each slice
+  onMouseOver: function() {
+    this.setState({text: '', opacity:'arc-hover'});
+    var percent = (this.props.data.value/this.props.total)*100;
+    percent = percent.toFixed(1);
+    this.setState({text: percent + " %"});
+  },
+
+  onMouseOut: function() {
+    this.setState({text: '', opacity:'arc'});
+  },
+    //onClick 
+  onClick: function() {
+    alert("You clicked "+this.props.name);
+  }
+});
+
+//////////// data series////////////
+var DataSeries = React.createClass({
+  propTypes: {
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    color: React.PropTypes.array,
+    data: React.PropTypes.array.isRequired,
+  },
+    render: function () {
+    
+    var color = this.props.colors;
+    var data = this.props.data;
+    data.sort(compare)
+    var width = this.props.width;
+    var height = this.props.height;
+    var pie = d3.layout.pie();
+    var result = data.map(function(item){
+      return item.count;
+    })
+    var names = data.map(function(item){
+      return item.name;
+    })
+    var sum = result.reduce(function(memo, num){ return memo + num; }, 0);
+    var position = "translate(" + (width)/2 + "," + (height)/2 + ")";
+    var bars = (pie(result)).map(function(point, i) {
+      return (
+        <Sector data={point} ikey={i} key={i} name={names[i]} colors={color} total=  
+        {sum} width={width} height={height}/>
+      )
+    });
+
+    return (
+        <g transform={position}>{bars}</g>
+    );
+  }
+});
+//////////// end data series////////////
+
+var D3Chart = React.createClass({
+  propTypes: {
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    children: React.PropTypes.node,
+  },
+  render: function() {
+    return (
+      <svg width={this.props.width} height={this.props.height}>        
+      {this.props.children}</svg>
+    );
+  }
+});
+
+var D3PieChart = React.createClass({
+  propTypes: {
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+    title: React.PropTypes.string,
+    data: React.PropTypes.array.isRequired,
+  },
+
+  getDefaultProps: function() {
+    return {
+      width: 300,
+      height: 350,
+      title: '',
+      Legend: true,
+    };
+  },
+
+  render: function() {
+ 
+    return (
+      <div>
+        <h4> {this.props.title} </h4>
+        <D3Chart width={this.props.width} height={this.props.height}>
+              <DataSeries data={this.props.data} colors={colors} width=
+                {this.props.width} height={this.props.height}/>
+        </D3Chart>
+        <D3Legend data={this.props.data} colors={colors} width={this.props.width - 100} height={this.props.height} />
+      </div>
+    );
+  }
+});
+
+
+
